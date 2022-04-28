67160157356c7f1346c2a93daed773468b3ce153
Hennry Chan <dual.hennry.chan@gmail.com>
2022-04-27 21:26:14 -0500

    Se creo el repositorio

---
+++ Regex/UserDefinedFuntions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
+using System.Text.RegularExpressions;
+
+    public class UserDefinefFunctions
+    {
+        public static readonly RegexOptions OptipnRegex = RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace;
+
+        [SqlFunction]
+        public static SqlBoolean RegexMatch(SqlChars input, SqlString pattern) => (SqlBoolean)new Regex(pattern.Value, UserDefinefFunctions.OptipnRegex).IsMatch(new string(input.Value));
+    }
